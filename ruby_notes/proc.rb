bdc1372573d25437cee433c7916e2765b74bad51
im <poson_hemp@yahoo.co.jp>
2020-06-08 08:40:33 +0900

    add: yeild

--- ruby_notes/proc.rb
+++ ruby_notes/proc.rb
@@ -12,3 +12,39 @@ proc_name.call
 
 name = :mimimi
 proc_name.call
+
+
+puts "################"
+
+
+# Yeild
+# ########
+
+# yeild 単独
+def yeild_sample
+  yield(1, 2, 3)
+end
+
+puts yeild_sample { |a, b, c| a + b + c }
+
+
+# proc と組み合わせ
+def yeild_proc(&proc)
+  proc.call(4, 5, 6)
+end
+
+puts yeild_proc { |a, b, c| a + b + c }
+
+
+# block を渡す
+def yeild_block
+  if block_given?
+    yield
+  else
+    puts "NOT given block."
+  end
+end
+
+yeild_block { puts "given block." }
+
+yeild_block
